799c3927cf48f6dde8c78dd820a3b843bc48d397
Jeremy Malara <jmalara@beachbody.com>
2016-09-08 14:26:59 -0700

    python

---
+++ assets/py/api-foreman.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
+print "test"
---
+++ test.py
@@ -0,0 +1,4 @@
+from mako.template import Template
+
+mytemplate = Template("hello world!")
+print(mytemplate.render())
