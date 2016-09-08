62dcbaaf22607696fcd44a6553abd7adf60ef57b
Jeremy Malara <jmalara@beachbody.com>
2016-09-08 15:05:59 -0700

    python

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+    return render_template('templates/template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
 
 if __name__ == '__main__':
