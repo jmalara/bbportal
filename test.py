b942dce65234bb69616339117b093f558b645491
Jeremy Malara <jmalara@beachbody.com>
2016-09-08 14:44:53 -0700

    python

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
+
+from mako.template import Template
 # -*- coding: UTF-8 -*-# enable debugging
 import cgitb
 cgitb.enable()
 print("Content-Type: text/html;charset=utf-8")
-from mako.template import Template
-
 mytemplate = Template("hello world!")
 print(mytemplate.render())
